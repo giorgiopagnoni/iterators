9ad9bab6cd7cd9a2001fcd1411deb115536d38c5
Giorgio Pagnoni <giorgio.pagnoni@sintattica.it>
2021-10-04 08:51:06 +0200

    double ended iterator

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,8 @@ where
     O::Item: IntoIterator,
 {
     outer: O,
-    inner: Option<<O::Item as IntoIterator>::IntoIter>,
+    front_iter: Option<<O::Item as IntoIterator>::IntoIter>,
+    back_iter: Option<<O::Item as IntoIterator>::IntoIter>,
 }
 
 impl<O> Flatten<O>
@@ -23,7 +24,8 @@ where
     fn new(iter: O) -> Self {
         Flatten {
             outer: iter,
-            inner: None,
+            front_iter: None,
+            back_iter: None,
         }
     }
 }
@@ -37,15 +39,42 @@ where
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            if let Some(ref mut inner_iter) = self.inner {
-                if let Some(i) = inner_iter.next() {
+            if let Some(ref mut front_iter) = self.front_iter {
+                if let Some(i) = front_iter.next() {
                     return Some(i);
                 }
-                self.inner = None;
+                self.front_iter = None;
             }
 
-            let next_inner_iter = self.outer.next()?.into_iter();
-            self.inner = Some(next_inner_iter);
+            if let Some(next_inner) = self.outer.next() {
+                self.front_iter = Some(next_inner.into_iter());
+            } else {
+                return self.back_iter.as_mut()?.next();
+            }
+        }
+    }
+}
+
+impl<O> DoubleEndedIterator for Flatten<O>
+where
+    O: Iterator + DoubleEndedIterator,
+    O::Item: IntoIterator,
+    <O::Item as IntoIterator>::IntoIter: DoubleEndedIterator,
+{
+    fn next_back(&mut self) -> Option<Self::Item> {
+        loop {
+            if let Some(ref mut back_iter) = self.back_iter {
+                if let Some(i) = back_iter.next_back() {
+                    return Some(i);
+                }
+                self.back_iter = None;
+            }
+
+            if let Some(next_back_inner) = self.outer.next_back() {
+                self.back_iter = Some(next_back_inner.into_iter());
+            } else {
+                return self.front_iter.as_mut()?.next_back();
+            }
         }
     }
 }
@@ -77,5 +106,49 @@ mod tests {
     #[test]
     fn two_wide() {
         assert_eq!(flatten(vec![vec!["a"], vec!["b"]]).count(), 2);
+    }
+
+    #[test]
+    fn reverse() {
+        assert_eq!(
+            flatten(std::iter::once(vec!["a", "b"]))
+                .rev()
+                .collect::<Vec<_>>(),
+            vec!["b", "a"]
+        );
+    }
+
+    #[test]
+    fn reverse_wide() {
+        assert_eq!(
+            flatten(vec![vec!["a"], vec!["b"]])
+                .rev()
+                .collect::<Vec<_>>(),
+            vec!["b", "a"]
+        );
+    }
+
+    #[test]
+    fn both_ends() {
+        let mut iter = flatten(vec![vec!["a1", "a2", "a3"], vec!["b1", "b2", "b3"]]);
+        assert_eq!(iter.next(), Some("a1"));
+        assert_eq!(iter.next_back(), Some("b3"));
+        assert_eq!(iter.next(), Some("a2"));
+        assert_eq!(iter.next_back(), Some("b2"));
+        assert_eq!(iter.next(), Some("a3"));
+        assert_eq!(iter.next_back(), Some("b1"));
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next_back(), None);
+    }
+
+    #[test]
+    fn infinite() {
+        let mut iter = flatten((0..).map(|i| 0..i));
+        // 0 => 0..0 => empty
+        // 1 => 0..1 => [0]
+        // 2 => 0..2 => [0, 1]
+        assert_eq!(iter.next(), Some(0));
+        assert_eq!(iter.next(), Some(0));
+        assert_eq!(iter.next(), Some(1));
     }
 }
